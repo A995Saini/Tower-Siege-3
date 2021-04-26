8539f024f8fdf94e8a89ca41f47c8add48214627
A995Saini <76857633+A995Saini@users.noreply.github.com>
2021-04-26 16:18:38 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var Ground 
+var Slingshot
+var Box
+var block1,block2,block3,block4,block5,block6,block7,block8
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
